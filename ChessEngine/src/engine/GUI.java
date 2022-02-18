6912f1139baeeed44d4dd0227f7844e7249f9d38
Shakir <beefthesavage@protonmail.com>
2022-02-18 22:26:45 +0000

    added an icon

--- ChessEngine/src/engine/GUI.java
+++ ChessEngine/src/engine/GUI.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -12,6 +13,7 @@ public class GUI extends Application {
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("Board.fxml"));
 		Scene scene = new Scene(root, 600, 600);
+		stage.getIcons().add(new Image("icon.png"));
 		stage.setTitle("Chess Engine");
 		stage.setScene(scene);
 		stage.show();
